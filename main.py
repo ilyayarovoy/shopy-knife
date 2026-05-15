5fb6dcac33d7e0b7185623d0cf17e5bfa2de3f70
ilyayarovoy <ponomorevilya@gmail.com>
2026-05-15 23:24:23 +0300

    fix main.py

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
+import os
 import logging
-from fastapi import FastAPI
-import uvicorn
-
+import traceback
 from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from alembic.config import Config
 from alembic import command
+import uvicorn
 
 from backend.api.routers import users, products
-from backend.database.engine import engine
-from backend.database.base import Base
-
-from fastapi.middleware.cors import CORSMiddleware
 
 logger = logging.getLogger(__name__)
+
 
 def run_migrations():
     base_dir = os.path.dirname(os.path.abspath(__file__))
     alembic_cfg = Config(os.path.join(base_dir, "alembic.ini"))
     command.upgrade(alembic_cfg, "head")
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    run_migrations()  # запускается при старте
+    run_migrations()
     yield
 
-app = FastAPI(title="AI Psychologist API")
+
+app = FastAPI(title="Knife Shop API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Разрешить все домены
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -42,9 +45,6 @@ app.include_router(products.router, prefix="/api")
 async def root():
     return {"status": "ok", "api": "running"}
 
-import traceback
-from fastapi import Request
-from fastapi.responses import JSONResponse
 
 @app.exception_handler(Exception)
 async def global_exception_handler(request: Request, exc: Exception):
@@ -55,7 +55,5 @@ async def global_exception_handler(request: Request, exc: Exception):
 
 
 if __name__ == '__main__':
-    import os
-
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
