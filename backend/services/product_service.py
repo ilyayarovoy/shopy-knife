e41af760b7a8c3beee92558b2f00345e2a1bcf17
ilyayarovoy <ponomorevilya@gmail.com>
2026-06-28 18:11:40 +0300

    fix error in add product

--- backend/services/product_service.py
+++ backend/services/product_service.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.repositories.product_repo import ProductRepository
+from backend.repositories.category_repo import CategoryRepository
 from backend.api.schemas.product_schemas import CreateProductSchema, UpdateProductSchema
 
 
@@ -9,6 +10,7 @@ class ProductService:
         self.session = session
 
         self.product_repo = ProductRepository(session=self.session)
+        self.category_repo = CategoryRepository(session=self.session)
 
 
     async def get_all_products_service(self, skip: int = 0, limit: int = 100, title: str | None = None, category_id: int | None = None, min_price: float | None = None, max_price: float | None = None):
@@ -32,6 +34,10 @@ class ProductService:
         return product
 
     async def create_new_product_service(self, product_data: CreateProductSchema):
+        category = await self.category_repo.get_category_by_id(product_data.category_id)
+        if not category:
+            return {"error": f"Category with id {product_data.category_id} not found"}
+
         new_product = await self.product_repo.create_product(
             category_id=product_data.category_id,
             title=product_data.title,
@@ -46,6 +52,11 @@ class ProductService:
         product = await self.product_repo.get_product_by_id(product_id=product_id)
         if not product:
             return None
+
+        if product_data.category_id:
+            category = await self.category_repo.get_category_by_id(product_data.category_id)
+            if not category:
+                return {"error": f"Category with id {product_data.category_id} not found"}
 
         updated_product = await self.product_repo.update_product(
             product,
